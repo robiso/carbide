4644cdfa4015644720b2a56e2ec64df00dc9f8e1
Robert Isoski <r@robiso.com>
2019-11-03 21:12:20 +0100

    Add files via upload

--- theme.php
+++ theme.php
@@ -1,156 +1,135 @@
+<?php global $Wcms ?>
 <!DOCTYPE html>
 <?php
-if(defined('VERSION') && !defined('version'))
-	define('version', VERSION);
-if(version<'2.0.0')
-	defined('INC_ROOT') OR die('Direct access is not allowed.');
+    if(defined('VERSION') && !defined('version'))
+    	define('version', VERSION);
+    if(version<'2.0.0')
+    	defined('INC_ROOT') OR die('Direct access is not allowed.');
 
-wCMS::addListener('menu', 'colorSelector');
+    $Wcms->addListener('menu', 'colorSelector');
 
-function colorSelector ($args) {
-	$args[0] .= '
-<li>
-	<ul class="color-selector">
-		<li><a href="" id="nightmode"><span class="fa fa-moon-o fa-lg" aria-hidden="true"></span></a></li><li><a href="" id="default"><span class="fa fa-sun-o fa-lg" aria-hidden="true"></span></a></li>
-	    <li><p><font size="-8">Theme</font></p></li>
-	</ul>
-</li>';
-	return $args;
-}
-if(isset($_COOKIE['stylesheet'])) {
-	switch($_COOKIE['stylesheet']) {
-		case 'nightmode':
-			$css = 'css/style-night.css';
-			break;
-		default:
-			$css = 'css/style-default.css';
-			break;
-	}
-} else {
-	$css = 'css/style-default.css';
-}
+    function colorSelector ($args) {
+    	$args[0] .= '
+            <li>
+            	<ul class="color-selector">
+            		<li><a href="" id="nightmode"><span class="glyphicon glyphicon-adjust" style="color: black;" aria-hidden="true"></span></a></li>
+            		<li><a href="" id="default"><span class="glyphicon glyphicon-adjust" style="color: white;" aria-hidden="true"></span></a></li>
+            	    <li><p><font size="-8">Theme</font></p></li>
+            	</ul>
+            </li>';
+    	return $args;
+    }
+
+    if(isset($_COOKIE['stylesheet'])) {
+        switch($_COOKIE['stylesheet']) {
+        	case 'nightmode':
+        		$css = 'css/style-night.css';
+        		break;
+        	default:
+        		$css = 'css/style-default.css';
+        		break;
+        }
+    } else {
+        $css = 'css/style-default.css';
+    }
 ?>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#75b1f2">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=wCMS::get('config','siteTitle')?> - <?=wCMS::page('title')?></title>
-	<meta name="description" content="<?=wCMS::page('description')?>">
-	<meta name="keywords" content="<?=wCMS::page('keywords')?>">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link id="stylesheet" rel="stylesheet" href="<?=wCMS::asset($css)?>">
+    <head>
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	
+        <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+        <meta name="description" content="<?= $Wcms->page('description') ?>">
+        <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
-	<?=wCMS::css()?>
+        <!--<link href="</? $Wcms->asset('favicon.ico') ?>" rel="shortcut icon" type="image/x-icon" />-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link id="stylesheet" rel="stylesheet" href="<?= $Wcms->asset($css) ?>">
+        <?= $Wcms->css() ?>
 
-</head>
-<body>
-	<div style="margin-top:65px">
-	<?=wCMS::alerts()?>
-	<?=wCMS::settings()?>
-    </div>
+    </head>
+    <body>
+    	<div style="margin-top:60px">
+            <?= $Wcms->alerts() ?>
+            <?= $Wcms->settings() ?>
 
-	<nav class="navbar navbar-light navbar-expand-lg fixed-top" id="thisNav" style="display:none;">
-		<div class="container-fluid colorBackground" style=" border-radius:4px;">
-		    <a class="navbar-brand site-title" href="<?=wCMS::url()?>" title="Home"></a>
-			<button class="navbar-toggler navbar-toggler-right collapsed no-border" type="button" data-toggle="collapse" data-target="#navbar-collapse-x" aria-controls="navbar-collapse-x" aria-expanded="false" aria-label="Toggle Nav">
-                <span> </span>
-                <span> </span>
-                <span> </span>
-            </button>
+        </div>
 
-			<div class="collapse navbar-collapse" id="navbar-collapse-x">
-				<ul class="nav navbar-nav ml-auto">
-					<?=wCMS::menu()?>
-				</ul>
-			</div>
-		</div>
-	</nav>
+    	<nav class="navbar navbar-inverse navbar-fixed-top">
+    		<div class="container-fluid css3-shadow colorBackground">
+    			<div class="navbar-header padLeft15">
+    				<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#navbar-collapse-x">
+    					<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+    				</button>
+    					<a class="navbar-brand" href="<?= $Wcms->url() ?>">
+    						<?= $Wcms->get('config', 'siteTitle') ?>
 
-	<div class="container-fluid">
-	    <div class="row sm-gutter">
-			<div class="col-xs-12 col-sm-8">
-			<div class="box css3-shadow whiteBackground padding10">
-				<?=wCMS::page('content')?>
-			    <?php if (wCMS::$currentPage == 'gallery'): ?>
-                <iframe src="https://www.powr.io/plugins/instagram-feed/view?unique_label=1aee9b35_1550449208&external_type=iframe" width="100%" height="870" frameborder="0"></iframe>
-                <?php endif ?>
-			</div>
-			</div>
-			<div class="col-xs-12 col-sm-4">
-			    <div class="box css3-shadow whiteBackground padding10">
-			        <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=1814967126/size=large/bgcol=333333/linkcol=0f91ff/tracklist=false/artwork=small/transparent=true/" seamless>
-			            <a href="http://carbide.bandcamp.com/album/house-of-snakes">House of Snakes by Carbide</a></iframe>
-                    <?=newEditableArea()?>
-                </div>
-			    <div class="box css3-shadow whiteBackground padding10">
-				    <?=wCMS::block('subside')?>
+    					</a>
+    			</div>
+    			<div class="collapse navbar-collapse" id="navbar-collapse-x">
+    				<ul class="nav navbar-nav navbar-right">
+                        <?= $Wcms->menu() ?>
 
+    				</ul>
+    			</div>
+    		</div>
+    	</nav>
 
-				    <div class="wrapper">
-				    <div class="links">
-                    <a href="https://open.spotify.com/artist/46FMfS2QJiBsbD42BYviN0"><span class="fa fa-spotify fa-lg padding7" style="color: #75b1f2;" aria-hidden="true"></span></a>
-                    <a href="https://instagram.com/carbide_band"><span class="fa fa-instagram fa-lg padding7" style="color: #75b1f2;" aria-hidden="true"></span></a>
-                    <a href="https://facebook.com/carbideband"><span class="fa fa-facebook-square fa-lg padding7" style="color: #75b1f2;" aria-hidden="true"></span></a>
-                    <a href="https://youtube.com/channel/UCqQJBHyLgElK4gSFF2d5d-g"><span class="fa fa-youtube-play fa-lg padding7" style="color: #75b1f2;" aria-hidden="true"></span></a>
-                    <a href="https://github.com/carbideband"><span class="fa fa-github fa-lg padding7" style="color: #75b1f2;" aria-hidden="true"></span></a>
-	    		    </div>
-		    	    </div>
-                </div>
-			</div>
-		</div>
+    	<div class="container-fluid">
+    		<div class="row-fluid sm-gutter">
+    			<div class="col-xs-12 col-sm-8">
+        			<div class="box css3-shadow whiteBackground padding10">
+                        <?= $Wcms->page('content') ?>
 
-	</div>
+        			</div>
+    			</div>
 
-	<footer class="container-fluid css3-shadow whiteFont colorBackground padding20">
-				<p><?= wCMS::footer() ?></p>
-	</footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <?=wCMS::js()?>
-    <script src="<?=wCMS::asset('js/js.cookie.js')?>"></script>
-	<script>
-		$( document ).ready(function() {
-			$('body').css('margin-bottom', $('footer').height()+'px');
-			
-			function change_stylesheet(c) {
-				switch(c) {
-					case 'nightmode':
-						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-night.css');
-						break;
-					case 'default':
-						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-default.css');
-						break;
-				}
-				Cookies.set("stylesheet", c, { expires: 7 });
-				$("#stylesheet").attr({href: stylesheet});
-			}
-			
-			if(Cookies.get("stylesheet")) {
-				change_stylesheet(Cookies.get("stylesheet"));
-			}
-			
-			$('.color-selector li a').click(function(e){
-				e.preventDefault();
-				change_stylesheet($(this).attr('id'));
-			});
-		    var prevScroll = 0;
+    			<div class="col-xs-12 col-sm-4">
+        			<div class="box css3-shadow whiteBackground padding10">
+                        <?= $Wcms->block('subside') ?>
 
-            $('#thisNav').fadeIn("fast");
+        			</div>
+    			</div>
+    		</div>
+    	</div>
 
-			$(window).scroll(function () {
-			    var currScroll = $('html').scrollTop();
+    	<footer class="container-fluid css3-shadow whiteFont colorBackground padding20">
+    		<p><?= $Wcms->footer() ?></p>
+    	</footer>
 
-			    if ((currScroll > 80) && (prevScroll < currScroll)) {
-			        $('#thisNav').fadeOut("fast");
-			    } else if (((currScroll < prevScroll) && (prevScroll - currScroll >= 7)) || (currScroll <= 7 )) {
-			        $('#thisNav').fadeIn("fast");
-			    }
-			    prevScroll = currScroll;
-			});
-		});
-	</script>
-</body>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <?= $Wcms->js() ?>
+
+    	<script src="<?= $Wcms->asset('js/js.cookie.js') ?>"></script>
+
+    	<script>
+    		$( document ).ready(function() {
+    			$('body').css('margin-bottom', $('footer').height()+'px');
+
+    			function change_stylesheet(c) {
+    				switch(c) {
+    					case 'nightmode':
+    						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-night.css');
+    						break;
+    					case 'default':
+    						var stylesheet = $('#stylesheet').attr('href').replace(/css\/style\-(.*)/g, 'css/style-default.css');
+    						break;
+    				}
+    				Cookies.set("stylesheet", c, { expires: 365 });
+    				$("#stylesheet").attr({href: stylesheet});
+    			}
+
+    			if(Cookies.get("stylesheet")) {
+    				change_stylesheet(Cookies.get("stylesheet"));
+    			}
+
+    			$('.color-selector li a').click(function(e){
+    				e.preventDefault();
+    				change_stylesheet($(this).attr('id'));
+    			});
+    		});
+    	</script>
+    </body>
 </html>
